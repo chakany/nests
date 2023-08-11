5c8510b35f94d74fb73916f6a9b6650f22b6a2f7
Jack Chakany <jack@chaker.net>
2023-08-11 18:58:05 -0400

    remove profane comments about perfect software

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,19 +17,14 @@
     onMount(async () => {
         const storedUser = window.sessionStorage.getItem('nestsCurrentUser');
         if (storedUser) {
-            const ndk = get(ndkStore)
 
             const newUser = new NDKUser({ npub: JSON.parse(storedUser).npub })
             newUser.ndk = ndk;
             currentUser.set(newUser);
-            // @ts-expect-error SHUT THE FUCK UP!
-            // why the fuck do i need to do this hacky bullshit? just add a pubkey field and use that to check if
-            // the user is authenticated, not this "promise" function async await bs. I honestly can't tell if there's a good reason for this
-            // or the ndk developer that wrote this wanted to accomplish this in a clever manner but it doesn't make
-            // any sense when it could have just been 85 times simpler to add a pubkey property to the class.
-            // TODO: Migrate this to it's own function! This doesn't feel right, but it should works.
+            // TODO: Migrate this to it's own function! This doesn't feel right, but it should work.
             ndk.signer = new NDKNip07Signer();
-            // this property wants the value of the output, not the function lmfao. took me a minute to figure that out.
+            // FYI: if authentication breaks after an ndk update this should be suspect no. 1
+            // @ts-expect-error this property wants the value of the output, not the function. took me a minute to figure that out.
             ndk.signer._userPromise = (async () => {
                 return newUser;
             })()
